28d2fbd6cc16e64137af3a4ab0f48be0a40061bb
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-06 10:51:21 -0600

    Add principal menu

---
+++ src-ui/my-gift/src/components/Options/Inicio/TextPrincipal.vue
@@ -0,0 +1,193 @@
+<template>
+  <section
+    class="text-container-prin"
+  >
+    <div class="text">
+      <q-item class="text-container">
+        <Transition 
+          appear
+          name="slide-fade"
+        >
+          <q-item-label>
+            Cada día es un regalo. Motivarnos a dejar el corazón en 
+            cada acción y enriquecer la vida de quienes amamos y nos rodean
+            debe volverse nuestro motivo...
+          </q-item-label>
+        </Transition>
+      </q-item>
+    </div>
+
+    <div>
+      <q-item>
+        <q-item-label class="thanks-message">
+          Thanks. God
+        </q-item-label>
+      </q-item>
+    </div>
+ 
+    <div
+      class="thanks-people"
+      id="thanks-people"
+    >
+      <q-scroll-area :style="`height: 200px; max-width: ${withScroll}px;`">
+        <div class="row no-wrap just-cent">
+          <div v-for="(message, index) in messages" :key="index" style="width: 150px" class="q-pa-sm">
+            <section
+              class="card-thanks"
+            >
+              <Transition
+                appear
+                name="simple-appear"
+              >
+                <section>
+                  <div class="person">
+                    <span class="name">{{ message.person }}</span>
+                  </div>
+                  
+                  <p class="message-text">
+                      {{ message.text }}
+                  </p>
+                </section>
+              </Transition>
+            </section>
+          </div>
+        </div>
+      </q-scroll-area>
+    </div>
+    
+  </section>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+
+onMounted(() => {
+  watchSize()
+  window.addEventListener('resize', watchSize)
+})
+
+const withScroll = ref(100)
+const watchSize = () => {
+  const element = document.getElementById('thanks-people');
+  withScroll.value = element.clientWidth
+};
+
+const messages = ref([
+  {
+    person: 'Mamá',
+    text: 'Me brindas tu apoyo, amor y consejos. que cada día me transforman'
+  },
+  {
+    person: 'Hermano, Hermana',
+    text: 'Son mis amigos y consejeros, hacen un trabajo maravilloso como hermanos.'
+  },
+  {
+    person: 'Rubiely',
+    text: 'Tu amor me ha hecho mucho bien. Llenas de alegría mi corazón. Tú eres el mejor regalo.'
+  }
+])
+
+</script>
+
+<style scoped>
+
+.text-container-prin {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 60% 5% 35%;
+}
+
+.text {
+  display: flex;
+  align-items: center;
+  margin-top: 4rem;
+  text-align: center;
+  font-size: 0.8rem;
+  padding: 0.5rem;
+}
+
+.text-container {
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  letter-spacing: 0.2rem;
+  color: var(--primary) !important;
+  padding: 3rem;
+}
+
+.slide-fade-enter-active {
+  transition: all 2s ease-out;
+  color: var(--secundary) !important;
+}
+
+.slide-fade-leave-active {
+  transition: all 2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+  color: var(--secundary) !important;
+}
+
+.thanks-message {
+  color: #8e8d8d;
+  margin-left: 1rem;
+}
+
+@media screen and (min-width: 420px) {
+  .thanks-people {
+    padding-right: 2rem;
+  }
+}
+
+.thanks-people {
+  width: 100%;
+  height: 100%;
+  display: inline;
+  padding-left: 2rem;
+  justify-content: center;
+}
+
+.person {
+  color: var(--color-text-2);
+  font-size: 0.7rem;
+  display: flex;
+  text-align: center;
+  width: 100%;
+  background: var(--primary);
+  padding-bottom: 0.5rem;
+  border-radius: 1rem;
+}
+
+.name {
+  margin-left: 1rem;
+  margin-top: 0.5rem;
+}
+
+.message-text {
+  font-size: 0.7rem; 
+  text-align: center;
+  padding: 1rem;
+  color: var(--secundary);
+}
+
+.just-cent {
+  justify-content: center;
+}
+
+.simple-appear-enter-active,
+.simple-appear-leave-active {
+  transition: opacity 3s ease;
+}
+
+.simple-appear-enter-from,
+.simple-appear-leave-to {
+  opacity: 0;
+}
+
+</style>
--- src-ui/my-gift/src/pages/IndexPage.vue
+++ src-ui/my-gift/src/pages/IndexPage.vue
@@ -5,17 +5,34 @@
     <left-menu></left-menu>
     
     <section>
+      <!--Vista principal-->
+      <web-view v-if="isSizeLaptop"></web-view>
+      <text-principal v-else></text-principal>
     </section>
   </q-page>
 </template>
 
 <script setup>
 
+import WebView from 'src/components/Options/Inicio/WebView.vue';
+import TextPrincipal from 'src/components/Options/Inicio/TextPrincipal.vue';
 import LeftMenu from 'src/components/Menu/LeftMenu.vue';
+import { ref, onMounted } from 'vue'
 
 defineOptions({
   name: 'IndexPage'
 });
+
+const isSizeLaptop = ref(true)
+onMounted(() => {
+  window.addEventListener('resize', watchSize)
+})
+
+const watchSize = () => {
+  const maxWidth = 620
+  isSizeLaptop.value = window.innerWidth > maxWidth
+}
+
 </script>
 
 <style scoped>
