aea4f6ffb4fd52a75eb78a1b693511e7f7d075f0
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-06 08:01:04 -0600

    Add left menu

---
+++ src-ui/my-gift/src/components/Help/ToolTipE.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-tooltip
+    anchor="center end" self="center start"
+  >
+    <span class="text">
+      {{ text }}
+    </span>
+  </q-tooltip>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  text: String
+})
+
+</script>
+
+
+<style scoped>
+
+.text {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+</style>
