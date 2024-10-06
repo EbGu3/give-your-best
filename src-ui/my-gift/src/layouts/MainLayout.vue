22f23a498fdac76de8d6c2e7357de06b17478cdf
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-06 06:51:51 -0600

    Add solution

---
+++ src-ui/my-gift/src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <!--Mi nombre-->
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
