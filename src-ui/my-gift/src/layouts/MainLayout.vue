a8bd066491ec8a738efebb8fe4f035dc3482b36b
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-06 07:06:38 -0600

    Add toolbar and my name

--- src-ui/my-gift/src/layouts/MainLayout.vue
+++ src-ui/my-gift/src/layouts/MainLayout.vue
@@ -1,8 +1,13 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <!--Mi nombre-->
+      <q-toolbar
+        class="toolbar"
+      >
+        <q-item>
+          <q-item-label class="by" caption>by</q-item-label>
+          <q-item-label class="my-name" caption>Jared. G</q-item-label>
+        </q-item>
       </q-toolbar>
     </q-header>
 
@@ -26,3 +31,29 @@ function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+
+
+<style scoped>
+
+.toolbar {
+  background: var(--secundary);
+  display: flex;
+  width: 100%;
+  justify-content: right;
+}
+
+.by {
+  margin-right: 0.6rem;
+  margin-top: 0.8rem;
+  color: var(--color-text-1);
+  font-size: 0.6rem;
+  letter-spacing: 0.3rem;
+}
+
+.my-name {
+  margin-top: 0.4rem !important;
+  color: var(--color-text-1);
+  letter-spacing: 0.2rem;
+}
+
+</style>
